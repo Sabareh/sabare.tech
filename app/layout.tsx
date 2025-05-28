9a7e38b75f3edaf0a27430d179698089e1d9ade7
sabareh <sabarevictor@gmail.com>
2025-05-28 10:10:54 +0300

    Fixing navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,7 +92,7 @@ export default function RootLayout({
 
           <div className="relative flex min-h-screen flex-col">
             <Navigation />
-            <main className="flex-1">{children}</main>
+            <main className="flex-1 pt-16">{children}</main>
             <Footer />
           </div>
           <Toaster />
